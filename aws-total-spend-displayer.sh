6a4af1d33d2016a4dbea2deeb25ad09f1b8a5476
anthony83 <nardoneanth83@gmail.com>
2021-04-09 13:00:27 +1000

    fixing if..else

--- aws-total-spend-displayer.sh
+++ aws-total-spend-displayer.sh
@@ -4,13 +4,18 @@
 
 # Capture Month-to-Date Spend Total using AWS CLI
 
-max=2
+max=1
+
 echo "AWS Max Spend is $max dollars"
 
-aws_billing_ammount_monthy=$(aws ce get-cost-and-usage --time-period Start=$(date -u -d "$TODAY" '+%Y-%m-01'),End=$(date -u +"%Y-%m-%d" --date="+1 day") --granularity MONTHLY --metrics UnblendedCost --output text | sort -r -k 3 | head -n 1 | cut -f 2)
+aws_billing_ammount_monthy=$(aws ce get-cost-and-usage \
+			--time-period Start=$(date -u -d "$TODAY" '+%Y-%m-01'),End=$(date -u +"%Y-%m-%d" --date="+1 day") \
+			--granularity MONTHLY \
+			--metrics UnblendedCost \
+			--output text | sort -r -k 3 | head -n 1 | cut -f 2)
 
 # Use free.currconv.com API to retrieve conversion from USD to AUD - FREE API key required, and added some transformation to obtain rate value
-conversion_value_usd_to_aud=$(curl -s -X GET 'https://free.currconv.com/api/v7/convert?q=USD_AUD&compact=ultra&apiKey=<API_KEY>' 2>&1 | sed -E 's/.*"USD_AUD":"?([^,"]*)"?.*/\1/' | sed 's/}//' )
+conversion_value_usd_to_aud=$(curl -s -X GET 'https://free.currconv.com/api/v7/convert?q=USD_AUD&compact=ultra&apiKey=<API_TOKEN>' 2>&1 | sed -E 's/.*"USD_AUD":"?([^,"]*)"?.*/\1/' | sed 's/}//' )
 
 # Multiply Monthly Spend total AUD into US Dollar 
 total=$(expr $aws_billing_ammount_monthy*$conversion_value_usd_to_aud | bc)
@@ -27,10 +32,14 @@ echo $convert | sed -E 's/ +/,/g' >> aws-total-spend.csv
 totaltoint=${total%%.*}
 RED='\033[0;31m'
 
-if  [ $totaltoint -gt $max ]
-then 
-	echo -e "${RED}AWS total spend is above max"
-else 
-	echo -e "AWS spend is below max"
+if  [[ "$totaltoint" -gt "$max" ]];
+	then 
+		echo -e "${RED}AWS total spend is above max"
+	else 
+		echo -e "AWS spend is below max"
 fi
+
+
+
+
 
